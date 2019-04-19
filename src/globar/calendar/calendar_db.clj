b9c1c55eed0c0ab78df9fd4fe00c15a7c2a587c8
Matthew Lee <laughew@gmail.com>
2019-04-19 15:38:51 -0700

    added calendar.core with initial fns and corresponding tests

--- src/globar/calendar/calendar_db.clj
+++ src/globar/calendar/calendar_db.clj
@@ -8,16 +8,16 @@
 (defn read-vendor-calendar-day
   "this function reads the database to return a map with the following structure:
    {:date string
-    :available (collection)
-    :booked (collection of time chunks)
+    :available (string representing time chunks)
+    :booked (string representing time chunks)
     :updated-at string}"
   [vendor-id date]
   (let [raw-result (db/query ["SELECT * FROM vendor_calendar WHERE vendor_id = ? AND date = ?" vendor-id date])
         result (first raw-result)]
     (println result)
     {:date (:date result)
-     :available (edn/read-string (:available_edn result))
-     :booked (edn/read-string (:booked_edn result))
+     :available (:available_edn result)
+     :booked (:booked_edn result)
      :updated-at (:updated_at result)}))                                    
     
 (defn upsert-vendor-calendar-day
@@ -25,29 +25,23 @@
    Existence is determined by looking for the presence of the :updated-at key.
    The format for calendar day is:
        {:date  string
-        :available (collection)
-        :booked (collection)
+        :available string
+        :booked string
         :updated-at string}
    Reads back the record and returns this map.  Note this will need to be a REST
    call as I believe GraphQL expects a nil?"
-
-  [vendor-id calendar-day]
-  (log/debug :fn :upsert-calendar-day :vendor vendor-id :calendar-day calendar-day)
-  (let [date (:date calendar-day)
-        available (:available calendar-day)
-        booked (:booked calendar-day)
-        updated-at (:update-at calendar-day)]
-    
-    (if (= updated-at nil)
+  [vendor-id date available booked updated-at]
+  (log/debug :fn :upsert-calendar-day :vendor vendor-id)
+  (if (= updated-at nil)
       (db/execute ["INSERT INTO vendor_calendar (vendor_id, date, available_edn, booked_edn)
-                 VALUES (?, ?, ?, ?)" vendor-id date (prn-str available) (prn-str booked)])
+                 VALUES (?, ?, ?, ?)" vendor-id date available booked])
       (db/execute ["UPDATE vendor_calendar SET available_edn = ?, booked_edn = ?
-                 WHERE vendor_id = ? AND updated_at = ?"] 
-                (prn-str available)
-                (prn-str booked)
+                 WHERE vendor_id = ? AND date = ? AND updated_at = ?" 
+                available
+                booked
                 vendor-id,
-                date,
-                updated-at)))
+                date
+                updated-at]))
   (log/debug :message "upsert-vendor-calendar-day completed")
-  (read-vendor-calendar-day vendor-id ( :date calendar-day)))
+  (read-vendor-calendar-day vendor-id date))
 
---
+++ test/globar/calendar_tests.clj
@@ -0,0 +1,22 @@
+(ns globar.calendar_tests
+  (:require [clojure.test :refer :all]
+            [globar.test_utils :refer [start-test-system! stop-test-system! q]]
+            [globar.calendar.core :as core]))
+
+(deftest test-calendar-read-write
+  (start-test-system!)
+  (let [vendor-id 1234
+        date "01/01/01"
+        test-map {:date date
+                  :available "((\"02:30\" \"05:00\") (\"23:50\" \"06:15\"))"
+                  :booked "((\"07:30\" \"08:00\") (\"13:20\" \"20:45\"))"}]
+    (core/write-calendar vendor-id test-map)
+    (let [written-map (core/read-calendar vendor-id date)]
+      (is (= (:date test-map) (:date written-map)))
+      (is (= (:available test-map (:available written-map))))
+      (is (= (:booked test-map (:booked written-map))))))
+  (stop-test-system!))
+
+
+
+
