7604b36dab22db6f3bce6fc8784b9ccf9d4d4d8e
Kevin Ye <kevinye88@gmail.com>
2019-06-26 00:13:05 +0000

    Created some booking tests

---
+++ test/globar/booking_tests.clj
@@ -0,0 +1,24 @@
+(ns globar.booking-tests
+  (:require [clojure.test :refer :all]
+            [globar.test_utils :refer [setup-test-system!]]
+            [globar.db :as db]
+            [globar.rest-api :as r-a]))
+
+
+(def booking-call {:vendor-id 1234 :user-id 1410 :time [0 1] :date "2019-07-18"})
+(def update-call {:cancelled true})
+
+(use-fixtures :each setup-test-system!)
+
+(deftest create-new-bookings
+  (let [{:keys [booking-id] :as booking1} (db/create-booking booking-call) ;creates new booking
+        found-booking (db/find-booking-by-id booking-id) ;looks up booking based off booking id
+        update-call (assoc update-call :booking-id booking-id)
+        _ (db/update-booking update-call) ;updates previously made booking
+        updated-booking (db/find-booking-by-id booking-id)
+        cancelled? (:cancelled updated-booking)]
+        (is (= booking1 found-booking))
+        (is (not= booking1 updated-booking))
+        (is (= cancelled? true)))
+  )
+  
