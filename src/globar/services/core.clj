44968061a49bb48d3ae580ee58d40a615d8d2a76
jay <jay@j3mc.ca>
2019-08-13 17:17:17 -0700

    some minor changes related to archon branch #83

--- src/globar/services/core.clj
+++ src/globar/services/core.clj
@@ -5,7 +5,8 @@
             [globar.services.db :as s-db]
             [globar.services.error-parsing :as s-ep]
             [globar.ven-reg.core :as vr-c]
-            [io.pedestal.http :as http]))
+            [io.pedestal.http :as http]
+            [io.pedestal.log :as log]))
 
 (s/def ::vendor-id
   ::vr-c/vendor-id)
@@ -32,21 +33,29 @@
 
 (defn get-services [request]
   "Returns the services for the vendor who issued the request"
-  (let [vendor (get-in request [:vendor])]
-    (http/json-response (db/list-services-for-vendor (:vendor-id vendor)))))
+  (let [vendor (get-in request [:vendor])
+        result (db/list-services-for-vendor (:vendor-id vendor))]
+    (log/debug :function "get-services" :result result)
+    (http/json-response result)))
 
 (defn upsert-service [request]
   "Create or modify a service definition record"
   (let [vendor (:vendor request) 
         service (-> (get-in request [:json-params :service])
                     (assoc :vendor-id (:vendor-id vendor)))]
+    (log/debug :function "upsert-service" :service service)
     (if (s/valid? ::valid-service service)  
-     (http/json-response (if (:service-id service) 
-                           (s-db/update-service service)
-                           (s-db/create-service service)))
-     (http/json-response {:error (->> service
-                                      (s/explain-str ::valid-service)
-                                      (ep/get-error-data ep/ERROR_MSG_SET_EN s-ep/get-error-code s-ep/ERROR_CODE_KEY))}))))
+      (do 
+        (let [result (if (= 0 (:service-id service)) 
+                       (s-db/create-service (dissoc service :service-id))
+                       (s-db/update-service service))]
+
+          (log/debug :function "upsert-service" :result result) 
+          (http/json-response {:data result})))
+     (do
+       (let [error-str (s/explain-str service ::valid-service)]
+         (log/debug :function "upsert-service" :error-string error-str)
+         (http/json-response {:error (ep/get-error-data error-str ep/ERROR_MSG_SET_EN s-ep/get-error-code s-ep/ERROR_CODE_KEY)}))))))
 
 
 
